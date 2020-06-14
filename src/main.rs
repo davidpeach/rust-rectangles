30d493a9c173d1feb80f6a6917df8f48250209cb
David Peach <mail@davidpeach.co.uk>
2020-06-14 10:08:25 +0100

    Add an associated function to create a square.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,17 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -14,9 +25,21 @@ fn main() {
         width: 30,
         height: 50,
     };
-
+    
     println!(
-        "The are of this rectangle is {} square pixels.", 
+        "The area of this rectangle is {} square pixels.", 
         rectangle.area()
     );
+
+    let rectangle2 = Rectangle {
+        width: 10,
+        height: 30,
+    };
+
+    println!("Can rectangle hold rectangle2 ? {}", rectangle.can_hold(&rectangle2));
+
+
+    let new_square = Rectangle::square(30);
+
+    println!("The is my new square height: {} and width: {}", new_square.height, new_square.width);
 }
